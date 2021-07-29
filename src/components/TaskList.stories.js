99ced30612a7bdc98f38c7374bafab9ffd9356a9
Florian Geierstanger <florian@geierstanger.org>
2021-07-29 10:14:35 +0200

    change decorator

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -6,7 +6,7 @@ import * as TaskStories from './Task.stories';
 export default {
   component: TaskList,
   title: 'TaskList',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+  decorators: [story => <div style={{ padding: '1rem' }}>{story()}</div>],
 };
 
 const Template = args => <TaskList {...args} />;
